ced9a81bc1576aa94f4ac1448d61bea71b4c873d
Lewis Dean <ldean@uac00027.internal.local>
2020-03-09 14:48:26 +0000

    remove add_dot from symlink

--- configurations/common/configuration.rb
+++ configurations/common/configuration.rb
@@ -5,5 +5,5 @@
 file_list = Dir.entries(pwd).select { |f| File.file?(f) }
 file_list.delete('configuration.rb')
 file_list.each do |file|
-  symlink "~/.#{file}", file, 1
+  symlink "~/.#{file}", file
 end
