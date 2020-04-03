aa44d4897e6d05074c248f79994e9a9deedd4492
Lewis Dean <ldean@ukcloud.com>
2020-04-03 15:16:28 +0100

    Remove TODO

--- configurations/common/configuration.rb
+++ configurations/common/configuration.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-# TODO: Symlink each file in common directory
-
+# Symlink each file in common directory
 file_list = Dir.entries(pwd).select { |f| File.file?(f) }
 file_list.delete('configuration.rb')
 file_list.each do |file|
